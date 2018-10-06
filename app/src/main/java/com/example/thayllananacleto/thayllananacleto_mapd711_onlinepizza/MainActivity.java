4714e1297c1eae1b2930295b3f43647ed3059cf8
thayllananacleto <thayllananacleto@gmail.com>
2018-10-06 16:47:05 -0400

    Home page and menu added.

--- app/src/main/java/com/example/thayllananacleto/thayllananacleto_mapd711_onlinepizza/MainActivity.java
+++ app/src/main/java/com/example/thayllananacleto/thayllananacleto_mapd711_onlinepizza/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thayllananacleto.thayllananacleto_mapd711_onlinepizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.pizzamenu, menu);
+        return true;
+    }
+
 }
